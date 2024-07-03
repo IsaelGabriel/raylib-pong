f82c131d9ff2059bd1e2567c46160897ee58e821
IsaelGabriel <isaelgsfaria@gmail.com>
2024-07-03 13:48:04 -0300

    Fix main scene instantiation

--- main.c
+++ main.c
@@ -6,18 +6,19 @@ const int WINDOW_HEIGHT = 600;
 const char* WINDOW_TITLE = "Pong";
 const int TARGET_FPS = 240;
 
-Scene* current_scene = generate_main_scene();
-
 int main(void) {
+    Scene* current_scene = generate_main_scene();
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 
     SetTargetFPS(TARGET_FPS);
 
+    current_scene->start();
     while(!WindowShouldClose()) {
+        current_scene->update();
         ClearBackground(BLACK);
 
         BeginDrawing();
-
+            current_scene->render();
             DrawFPS(0, 0);
         EndDrawing();
     }
--- main_scene.c
+++ main_scene.c
@@ -1,16 +1,6 @@
+#include <stdlib.h>
 #include "scenes.h"
 #include "raylib.h"
-
-Scene* generate_main_scene() {
-    Scene* scene = malloc(sizeof(Scene));
-
-    scene->type = MAIN_SCENE;
-    scene->start = start;
-    scene->update = update;
-    scene->render = render;
-
-    return scene;
-}
 
 void start() {
 
@@ -22,4 +12,15 @@ void update() {
 
 void render() {
     DrawRectangle(20, 20, 150, 150, RED);
+}
+
+Scene* generate_main_scene() {
+    Scene* scene = malloc(sizeof(Scene));
+
+    scene->type = MAIN_SCENE;
+    scene->start = start;
+    scene->update = update;
+    scene->render = render;
+
+    return scene;
 }
